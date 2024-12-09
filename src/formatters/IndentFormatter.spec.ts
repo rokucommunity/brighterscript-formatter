a963b9a343adb1bf1ddeccd770c764a4bc7a7bae
philanderson888 <philanderson888@hotmail.com>
2024-12-09 09:29:44 +0000

    Remove commented-out tests from IndentFormatter.spec.ts

--- src/formatters/IndentFormatter.spec.ts
+++ src/formatters/IndentFormatter.spec.ts
@@ -9,65 +9,6 @@ describe('IndentFormatter', () => {
         formatter = new IndentFormatter();
     });
 
-    describe('nullTest', () => {
-        it('null test', () => {
-            expect(null).to.eql(null);
-        });
-    });
-
-    describe('ensureTokenIndentation', () => {
-        it('does nothing for empty or invalid tokens', () => {
-            expect(
-                formatter['ensureTokenIndentation'](null as any, 0)
-            ).to.eql(null);
-            expect(
-                formatter['ensureTokenIndentation']([], 0)
-            ).to.eql([]);
-        });
-
-        it('handles negative tab size', () => {
-            expectTokens(
-                formatter['ensureTokenIndentation'](lex(`\tspeak()`), -2),
-                ['', 'speak', '(', ')']
-            );
-        });
-
-        it('does not add whitespace token if no indentation is needed', () => {
-            expectTokens(
-                formatter['ensureTokenIndentation'](lex(`speak()`), 0),
-                ['speak', '(', ')']
-            );
-        });
-
-        it('dedupes side-by-side whitespace tokens into one', () => {
-            expectTokens(
-                formatter['ensureTokenIndentation'](lex(` \t speak()`), 1),
-                ['    ', 'speak', '(', ')']
-            );
-        });
-
-        it('adds whitespace when missing', () => {
-            expectTokens(
-                formatter['ensureTokenIndentation'](lex(`speak()`), 1),
-                ['    ', 'speak', '(', ')']
-            );
-        });
-
-        it('adds correct indentation when missing', () => {
-            expectTokens(
-                formatter['ensureTokenIndentation'](lex(`speak()`), 3),
-                ['            ', 'speak', '(', ')']
-            );
-        });
-
-        it('uses supplied indentation char when provided', () => {
-            expectTokens(
-                formatter['ensureTokenIndentation'](lex(`speak()`), 3, '\t'),
-                ['\t\t\t', 'speak', '(', ')']
-            );
-        });
-    });
-
     describe('ensureTokenIndentation', () => {
         it('does nothing for empty or invalid tokens', () => {
             expect(
